eef835614d1dc0f43e51bf1540ca6236d3ef504c
jad <jad.bathore@gmail.com>
2024-10-25 19:37:53 +0200

    update cli 2

--- app/threeElement/Setting/configImport.js
+++ app/threeElement/Setting/configImport.js
@@ -1,8 +1,4 @@
-const { THREE,
-	OrbitControls, glb, gltf,
-	hdr, img, renderer,
-	scene, camera, orbit,
-	loader, } = require('../../public/versionning/linkFile.js')import * as THREE from 'three'
+import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { ImageCache,ImagesCacheHandler } from '../../cache/cacheImageUtility.js'
 
